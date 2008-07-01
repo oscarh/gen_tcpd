2073c7a85639c7869836a7bf68d25d84ed538909
martin@valhall.local <none@none>
2008-07-01 19:39:19 +0200

    Changed behaviour to match gen better

--- src/gen_tcpd.erl
+++ src/gen_tcpd.erl
@@ -2,11 +2,11 @@
 -behaviour(gen_server).
 
 -export([start_link/5]).
--export([send/2, recv/3, close/1]).
+-export([send/2, recv/3, close/1, controlling_process/2]).
 -export([init/1, terminate/2]).
 -export([handle_call/3, handle_cast/2, handle_info/2]).
 -export([code_change/3]).
--export([acceptor_loop/3]).
+-export([acceptor_loop/2]).
 -export([behaviour_info/1]).
 
 -record(state, {callback, acceptor, socket}).
@@ -18,7 +18,6 @@ start_link(Callback, CallbackArgs, ssl, Port, Options) ->
 	gen_server:start_link(?MODULE, 
 		[ssl, {Callback, CallbackArgs}, Port, Options], []).
 
-
 recv({Mod, Socket}, Size, Timeout) ->
 	Mod:recv(Socket, Size, Timeout).
 
@@ -28,54 +27,60 @@ send({Mod, Socket}, Packet) ->
 close({Mod, Socket}) ->
 	Mod:close(Socket).
 
-init([Type, Callback, Port, Options]) ->
+controlling_process({Mod, Socket}, Pid) ->
+	Mod:controlling_process(Socket, Pid).
+
+init([Type, {Mod, Args}, Port, Options]) ->
 	process_flag(trap_exit, true),
+	{ok, CState} = Mod:init(Args),
 	{ok, Socket} = listen(Type, Port, Options),
-	{ok, Acceptor} = acceptor(Socket, Callback),
-	{ok, #state{callback = Callback, socket = Socket, acceptor = Acceptor}}.
+	{ok, Acceptor} = acceptor(Socket),
+	{ok, #state{
+		callback = {Mod, CState}, 
+		socket = Socket,
+		acceptor = Acceptor
+	}}.
 
-handle_call(_, _From, State) ->
-	{reply, ok, State}.
+handle_call({new_connection, Socket}, _From, State) ->
+	{CMod, CState} = State#state.callback,
+	{Reply, CState0} = CMod:handle_connection(Socket, CState),
+	{reply, Reply, State#state{callback = {CMod, CState0}}}.
 
-handle_cast({spawn_acceptor, Pid}, State) when Pid == State#state.acceptor ->
-	{ok, Acceptor} = acceptor(State#state.socket, State#state.callback),
-	{noreply, State#state{acceptor = Acceptor}};
 handle_cast(_, State) ->
 	{noreply, State}.
 
-handle_info({'EXIT', _, normal}, State) ->
-	{noreply, State};
 handle_info({'EXIT', Pid, _}, State) when Pid == State#state.acceptor ->
-	{ok, Acceptor} = acceptor(State#state.socket, State#state.callback),
+	{ok, Acceptor} = acceptor(State#state.socket),
 	{noreply, State#state{acceptor = Acceptor}};
-handle_info(_, State) ->
-	{noreply, State}.
+handle_info(Info, #state{callback = {CMod, CState}} = State) ->
+	{noreply, CState0} = CMod:handle_info(Info, CState),
+	{noreply, State#state{callback = {CMod, CState0}}}.
 
-terminate(_, State) ->
+terminate(Reason, #state{callback = {CMod, CState}} = State) ->
 	close(State#state.socket),
-	ok.
+	CMod:terminate(Reason, CState).
+
 code_change(_, _, State) ->
 	{ok, State}.
 
-acceptor(Socket, Callback) ->
+acceptor(Socket) ->
 	Parent = self(),
 	{ok, spawn_link(fun() -> 
-		case catch ?MODULE:acceptor_loop(Parent, Socket, Callback) of
-			{'EXIT', Reason} -> exit(Reason); %% Prevent SASL error reports...
+		case catch ?MODULE:acceptor_loop(Parent, Socket) of
+			%% Prevent SASL error reports...
+			{'EXIT', Reason} -> exit(Reason); 
 			Result           -> Result
 		end
 	end)}.
 
-acceptor_loop(Parent, Socket, {Callback, CallbackArgs}) ->
-	case Callback:init(CallbackArgs) of
-		{ok, CallbackState} ->
-			{ok, Client} = accept(Socket),
-			gen_server:cast(Parent, {spawn_acceptor, self()}),
-			unlink(Parent),
-			% FIXME Hey, no one is supervising this process!
-			Callback:handle_connection(Client, CallbackState);
-		Other ->
-			exit({bad_return, Other})
+acceptor_loop(Parent, Socket) ->
+	{ok, Client} = accept(Socket),
+	ok = controlling_process(Client, Parent),
+	case gen_server:call(Parent, {new_connection, Client}) of
+		noreply ->
+			acceptor_loop(Parent, Socket);
+		stop ->
+			exit(normal)
 	end.
 
 listen(Mod, Port, Options) ->
@@ -91,6 +96,6 @@ accept({Mod, Socket}) ->
 	end.
 
 behaviour_info(callbacks) ->
-	[{init, 1}, {handle_connection, 2}];
+	[{init, 1}, {handle_connection, 2}, {handle_info, 2}, {terminate, 2}];
 behaviour_info(_) ->
 	ok.
